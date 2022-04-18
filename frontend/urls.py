ab47f8d850119170e524682dd162f107dfe8c7e6
Bernardo Issenguel <beissenguel12@gmail.com>
2022-04-18 13:53:16 +0100

    login

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
 
 app_name = 'tasks'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('accounts/new/', views.CreateAccountView.as_view(), name='new-account'),
     path('accounts/create/', views.register_user, name='create-account'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html',
+                                                         extra_context={'form': AuthenticationForm()}), name='login')
 ]
