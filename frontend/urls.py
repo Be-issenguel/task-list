761367a9023b9f1b35e2dac798e911a2e1a8c7ab
Bernardo Issenguel <beissenguel12@gmail.com>
2022-04-18 13:12:29 +0100

    create user

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'tasks'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('accounts/new/', views.CreateAccountView.as_view(), name='new-account'),
+    path('accounts/create/', views.register_user, name='create-account'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.forms import UserCreationForm
 
 
-# Create your views here.
+# Tasks views.
 class IndexView(View):
     def get(self, request):
         return render(request, 'frontend/index.html')
+
+
+# Accounts views
+class CreateAccountView(View):
+    def get(self, request):
+        ctx = {
+            'form': UserCreationForm(),
+        }
+        return render(request, 'frontend/accounts-new.html', context=ctx)
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            ctx = {
+                'form': UserCreationForm(),
+                'errors': form.errors,
+            }
+            return render(request, 'frontend/accounts-new.html', context=ctx)
+    else:
+        ctx = {
+            'form': UserCreationForm(),
+        }
+        return render(request, 'frontend/accounts-new.html', context=ctx)
