d3e9c0ba50971c319e8c25be3d779d9d6f621ba0
Bernardo Issenguel <beissenguel12@gmail.com>
2022-04-18 18:50:22 +0100

    create model and migrations

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from config.settings import MEDIA_ROOT
+
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name='task name')
+    description = models.TextField(verbose_name='task description')
+    limit_date = models.DateField(verbose_name='task limit date'),
+    priority = models.IntegerField(default=1, verbose_name='task priority')
+    is_finished = models.BooleanField(default=False, verbose_name='task is finished')
+
+
+class Attachment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name='attachment name')
+    file = models.FileField(upload_to=MEDIA_ROOT, verbose_name='attachment file')
