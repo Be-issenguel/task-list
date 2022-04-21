2323b2f9395aad3ef971ef4b185fdbca3edbe0d9
Bernardo Issenguel <beissenguel12@gmail.com>
2022-04-21 11:00:44 +0100

    rewrite attachment form

--- frontend/forms.py
+++ frontend/forms.py
@@ -23,6 +23,11 @@ class TaskForm(ModelForm):
 
 
 class AttachmentForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['file'].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Attachment
         exclude = ['task']
