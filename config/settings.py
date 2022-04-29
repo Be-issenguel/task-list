846bf0c2f460db6b853573c0c87c3f80523de115
Bernardo Issenguel <beissenguel12@gmail.com>
2022-04-29 01:46:23 +0100

    removing slash from MEDIA_URL

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '.media/attachments')
-MEDIA_URL = '/media/a   ttachments/'
+MEDIA_URL = 'media/attachments/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
